ef233aeef69c45e56f4ef72d914a6e8c195b1704
SilviaCastagnino <sylvia19976@gmail.com>
2026-01-31 19:01:54 +0100

    New Changes

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'A simple API to manage contacts'
   },
   host: 'localhost:3000',
-  schemes: ['http', 'https'],
+  schemes: ['https', 'http'],
 };
 
 const outputFile = './swagger-output.json';
